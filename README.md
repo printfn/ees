3386a14fa4ee41f040fc99fa07e89e9b900b3425
printfn <printfn@users.noreply.github.com>
2021-05-27 22:49:36 +1200

    Add documentation

---
+++ README.md
@@ -0,0 +1,33 @@
+# ees: Simple Error-Handling Library
+
+`ees` is a simple error-handling library. Rather than provide its own error-related
+types, it uses `std::error::Error` and provides a number of convenience functions.
+
+```rust
+use std::io::Read;
+
+// Use ees::Error for arbitrary owned errors
+fn do_work() -> Result<(), ees::Error> {
+    let mut file = std::fs::File::open("hello world")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    if contents.is_empty() {
+        // Construct an error on the fly with a given message
+        return Err(ees::error_with_message("file is empty"));
+    }
+    Ok(())
+}
+
+// ees::ErrorRef<'_> represents an arbitrary borrowed error
+fn take_an_error(error: ees::ErrorRef<'_>) {
+    // Print the complete error chain
+    println!("Error: {}", ees::print_error_chain(error));
+}
+
+// Use ees::MainResult to automatically create nicely-formatted error messages
+// in the main() function
+fn main() -> ees::MainResult {
+    do_work()?;
+    Ok(())
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,36 @@
 //! This library is intended to provide simple error-handling-related helper functions
 //! and types. Rather than provide its own error-related types, it is centered around
 //! the [std::error::Error] type.
+//!
+//! Usage:
+//! ```no_run
+//! use std::io::Read;
+//!
+//! // Use ees::Error for arbitrary owned errors
+//! fn do_work() -> Result<(), ees::Error> {
+//!     let mut file = std::fs::File::open("hello world")?;
+//!     let mut contents = String::new();
+//!     file.read_to_string(&mut contents)?;
+//!     if contents.is_empty() {
+//!         // Construct an error on the fly with a given message
+//!         return Err(ees::error_with_message("file is empty"));
+//!     }
+//!     Ok(())
+//! }
+//!
+//! // ees::ErrorRef<'_> represents an arbitrary borrowed error
+//! fn take_an_error(error: ees::ErrorRef<'_>) {
+//!     // Print the complete error chain
+//!     println!("Error: {}", ees::print_error_chain(error));
+//! }
+//!
+//! // Use ees::MainResult to automatically create nicely-formatted error messages
+//! // in the main() function
+//! fn main() -> ees::MainResult {
+//!     do_work()?;
+//!     Ok(())
+//! }
+//! ```
 
 use std::{error, fmt};
 
@@ -79,6 +109,34 @@ pub fn add_message(error: Error, message: &'static str) -> Error {
     }
     .into()
 }
+
+/// This type wraps an arbitrary error, and is intended for use in the `main()` method
+pub struct MainError {
+    error: Error,
+}
+
+impl fmt::Display for MainError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error: {}", print_error_chain(self.error.as_ref()))
+    }
+}
+
+impl fmt::Debug for MainError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error: {}", print_error_chain(self.error.as_ref()))
+    }
+}
+
+impl<E: Into<Error>> From<E> for MainError {
+    fn from(error: E) -> Self {
+        Self {
+            error: error.into(),
+        }
+    }
+}
+
+/// A convenient way to return arbitrary errors from `main()`
+pub type MainResult = Result<(), MainError>;
 
 #[cfg(test)]
 mod tests {
