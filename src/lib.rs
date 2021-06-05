c6744c949513aefda67b7278902bb958c26e8cf7
printfn <printfn@users.noreply.github.com>
2021-06-05 18:29:42 +1200

    Use multiline formatting for MainError/MainResult

--- src/lib.rs
+++ src/lib.rs
@@ -102,13 +102,13 @@ pub struct MainError {
 
 impl fmt::Display for MainError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", print_error_chain(self.error.as_ref()))
+        write!(f, "{:#}", print_error_chain(self.error.as_ref()))
     }
 }
 
 impl fmt::Debug for MainError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", print_error_chain(self.error.as_ref()))
+        write!(f, "{:#}", print_error_chain(self.error.as_ref()))
     }
 }
 
